0e85da85e8dda7be3c1aff43e86d82d47fa0133f
LasSparrow <thelassparrow@gmail.com>
2021-03-26 10:19:20 -0700

    m

--- server.js
+++ server.js
@@ -9,12 +9,9 @@ app.use('/', function (req, res, next) {
   console.log(origin);
   if(allowedOrigins.indexOf(origin) > -1){}
 
-  header("Access-Control-Allow-Origin: *");
-  header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-  header("Access-Control-Allow-Headers: Authorization");
   res.setHeader('Access-Control-Allow-Origin', origin || "*");
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+  res.setHeader('Access-Control-Allow-Headers', 'Authorization, Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
   res.setHeader('Access-Control-Allow-Credentials', true);
