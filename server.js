0237ea5dc07caea2555bfc89af59a4e8a049b5d0
LasSparrow <thelassparrow@gmail.com>
2021-03-25 18:57:37 -0700

    ??

--- server.js
+++ server.js
@@ -6,9 +6,8 @@ const app = express()
 app.use('/', function (req, res, next) {
   var allowedOrigins = ['http://localhost:3000', 'http://localhost:6006', 'http://localhost:8080', "https://*"];
   var origin = req.headers.origin;
-  //console.log(origin);
-  /*if(allowedOrigins.indexOf(origin) > -1){
-  }*/
+  console.log(origin);
+  if(allowedOrigins.indexOf(origin) > -1){}
 
   res.setHeader('Access-Control-Allow-Origin', origin || "*");
   // Request methods you wish to allow
