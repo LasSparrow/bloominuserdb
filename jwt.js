75653efa40592ed1f0dc62b44594d54f4d787473
LasSparrow <thelassparrow@gmail.com>
2021-03-27 16:38:54 -0700

    ?

--- jwt.js
+++ jwt.js
@@ -8,3 +8,29 @@ function generateToken(data) {
 }
 exports.generateToken = generateToken
 //test
+
+
+// Custom jwt middleware function
+function authorize(req, res, next) {
+    const authHeader = req.headers.authorization
+    const token = authHeader ? authHeader.split(' ')[1] : null
+  
+    if (!token) {
+      console.error("no token sent to server")
+      res.status(401).send({error: "no token sent to server"})
+      return 
+    }
+  
+    let decoded
+    try {
+      decoded = jwt.verify(token, secret);
+    } catch(error) {
+      console.error(error)
+      res.status(403).send({error: "Invalid Token"})
+      return
+    }
+  
+    req.user = decoded
+    next()
+  }
+  exports.authorize = authorize
